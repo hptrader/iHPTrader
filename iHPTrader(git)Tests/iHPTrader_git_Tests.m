197c9cd0818b599787f3aa6fad7c25927269c063
Hugo <Hugo@Hugos-MacBook-Pro.local>
2014-06-14 22:24:47 +0800

    Initial commit from local git

---
+++ iHPTrader(git)Tests/iHPTrader_git_Tests.m
@@ -0,0 +1,34 @@
+//
+//  iHPTrader_git_Tests.m
+//  iHPTrader(git)Tests
+//
+//  Created by Hugo on 14/6/14.
+//  Copyright (c) 2014 Hugo Home. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iHPTrader_git_Tests : XCTestCase
+
+@end
+
+@implementation iHPTrader_git_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
